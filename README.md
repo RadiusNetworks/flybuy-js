86b8ad33aacfde268476363f556d18efc38fe315
Scott Newman <snewman18@gmail.com>
2021-04-30 12:47:32 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# flybuy-js
-Flybuy Javascript SDK 
+# Flybuy Javascript SDK
+
+The Flybuy JS SDK allows partners to draw a map with markers for orders and polygons for premises and pickup areas.
+
+## Supported Map Providers
+
+Currently, the SDK only supports Google Maps. (Mapbox will be added in the future)
+
+To use Google Maps, you *must* include the Google Maps JS SDK in your HTML before you include the Flybuy JS:
+```
+<script src="https://maps.googleapis.com/maps/api/js?key=<GOOGLE MAPS API KEY"></script>
+<script src="flybuy.js"></script>
+```
+
+_Note: The Flybuy JS SDK will automatically determine that you want to use Google Maps if you have included the Google Maps Javascript._
+
+## Creating a map
+
+First, instantiate a new `Flybuy` object. After you have done that, you can call `flybuy.createMap` once you have the data you wish to load. The first argument to the method should the selector string. (not a DOM element)
+
+```
+let flybuy = new Flybuy();
+
+window.addEventListener('DOMContentLoaded', () => {
+  let jsonFile = 'sample_data/initial_orders_api.json';
+
+  fetch(jsonFile).then(response => response.json()).then(data => {
+    flybuy.createMap('div#map', data);
+  });
+});
+```
+
+_Note: The DOM must be loaded before you attempt to create the map. The example uses the `DOMContentLoaded` event, but you could also move the `<script>` tag after the body._
+
+## Updating a map
+
+If you want to update a map after it has been drawn, you can call `flybuy.update`:
+```
+let jsonFile = 'sample_data/orders_api_update.json';
+
+fetch(jsonFile).then(response => response.json()).then(data => {
+  flybuy.update(data);
+});
+```
+
+## Features not implemented yet
+
+* The `update` method will move existing markers but it will not remove markers from the map
+* The `update` method can accept new polygons for pickup areas and premises, but it will not draw them yet
---
+++ flybuy.js
@@ -0,0 +1,315 @@
+class Flybuy {
+
+  constructor() {
+    this.map = null;
+    this.provider = null;
+    this.markers = [];
+    this._premisesFeatureId;
+    this._pickupAreaFeatureIds = [];
+  }
+
+  // PUBLIC METHODS
+
+  createMap(containerSelector, payloadData, callback) {
+    // Try to determine which map provider (Google, Mapbox, etc.) is being used
+    this.provider = this._determineMapProvider();
+
+    if (!this.provider) {
+      throw new Error('Could not determine map provider.');
+    }
+
+    // Make sure the access token is provided if needed
+    if (!this._checkAccessToken(this.provider)) {
+      throw new Error('Access token for provider not found. Aborting.');
+    }
+
+    // Find the DOM element where the map should be drawn
+    let containerElement = this._findContainerElement(containerSelector);
+
+    if (!containerElement) {
+      throw new Error('Container element for map selector not found. Aborting.');
+    }
+
+    // Create a no-op function if a callback was not provided
+    callback = callback || function() {};
+
+    // Attempt to draw the map for the provider
+    this.map = this._initMapForProvider(this.provider, containerElement, payloadData.data)
+      .then(map => {
+        this.map = map;
+        this._drawPremises(payloadData.data);
+        this._drawPickupAreas(payloadData.data);
+        this._updateOrders(payloadData, true);
+        this._styleMap();
+        callback(true);
+      })
+      .catch(error => {
+        console.error(error);
+        callback(false);
+      });
+  }
+
+  removeMap() {
+    if (this.provider === 'google') {
+      this.map = null;
+    }
+    else if (this.provider === 'mapbox') {
+      this.map.remove();
+    }
+  }
+
+  update(data) {
+    this._updateOrders(data);
+  }
+
+  // PRIVATE METHODS
+
+  _updateOrders(payloadData, initialUpdate=false) {
+    if (!this.map) {
+      throw new Error('You must call createMap before attempting to update orders.');
+    }
+
+    let orders = [];
+
+    // Data came from a webhook
+    if (payloadData && payloadData.data && payloadData.data.type === 'order') {
+      orders = [payloadData.data]; // make an array of one object
+    }
+    // Data came from orders API
+    else if (payloadData && payloadData.data && payloadData.data.orders && Array.isArray(payloadData.data.orders)) {
+      orders = payloadData.data.orders;
+    }
+
+    orders.forEach(order => {
+      let markerObj = this._findMarkerByOrderId(order.id);
+
+      // Create a new marker if we don't have one for this order
+      if (!markerObj) {
+        let newMarker = this._createMarker(order.customer_latitude, order.customer_longitude, this.map);
+        this.markers.push({'orderId': order.id, 'marker': newMarker});
+      }
+      // Update the marker if we already have one
+      else {
+        this._moveMarker(markerObj.marker, order.customer_latitude, order.customer_longitude);
+      }
+    });
+
+    // If there was a site object passed in, try to redraw the polygons (unless it's the initial update)
+    if (!initialUpdate && payloadData && payloadData.data && payloadData.data.site) {
+      console.warn('Redrawing polygons in _updateOrders is not yet supported');
+    }
+
+    // Center the map on the bounds of all the markers
+    this._centerMap();
+  }
+
+  _findContainerElement(selector) {
+    let element = document.querySelector(selector);
+
+    if (!element) {
+      throw new Error(`Unable to find DOM element for selector: "${selector}"`);
+    }
+    else {
+      return element;
+    }
+  }
+
+  _initMapForProvider(provider, containerElement, data) {
+    return new Promise((resolve, reject) => {
+      let lat = 0.0;
+      let lng = 0.0;
+
+      // Attempt to determine the center point from the data
+      if (data.site && data.site.location.lat && data.site.location.lng) {
+        lat = parseFloat(data.site.location.lat);
+        lng = parseFloat(data.site.location.lng);
+      }
+
+      if (provider === 'google') {
+        let centerPoint = new google.maps.LatLng(lat,lng);
+        let map = new google.maps.Map(containerElement);
+        let bounds = new google.maps.LatLngBounds();
+        bounds.extend(centerPoint);
+        map.fitBounds(bounds);
+
+        resolve(map);
+      }
+      else if (provider === 'mapbox') {
+        throw new Error('Mapbox is not yet supported.');
+      }
+      else {
+        reject('Could not create map for provider');
+        return;
+      }
+    });
+  }
+
+  // If the provider requres a key or token, make sure it's been provided
+  _checkAccessToken(provider) {
+    if (provider === 'mapbox') {
+      return (mapboxgl.accessToken !== undefined && mapboxgl.accessToken !== null);
+    }
+    else {
+      return true;
+    }
+  }
+
+  // Looks for window variables to determine which map library has been used
+  _determineMapProvider() {
+    if (typeof(google) === 'object') {
+      return 'google';
+    }
+    else if (typeof(mapboxgl) === 'object') {
+      return 'mapbox';
+    }
+    else {
+      return null;
+    }
+  }
+
+  // Draw the premises polygon on the map (if it exists)
+  _drawPremises(data) {
+    if (!this.map) {
+      throw new Error('You must call createMap before attempting to draw a premises feature.');
+    }
+
+    // Attempt to find the premises polygon from the data provided
+    if (data.site && data.site.premises_area) {
+      let premisesFeature = data.site.premises_area;
+      let premisesGeoJson = {'type': 'FeatureCollection', 'features': [premisesFeature]};
+      this._premisesFeatureId = premisesFeature.id;
+
+      if (this.provider === 'google') {
+        this.map.data.addGeoJson(premisesGeoJson);
+        this._centerMap();
+      }
+      else {
+        console.error('Adding premises for provider not yet supported');
+      }
+    }
+    else {
+      console.warn('Could not find premises feature in the data provided');
+    }
+  }
+
+  // Centers the map based on the features that have been added
+  _centerMap() {
+    if (!this.map) {
+      throw new Error('You must call createMap before you attempt to call _recenterMap');
+    }
+
+    let bounds = new google.maps.LatLngBounds();
+
+    this.markers.forEach(markerObj => {
+      bounds.extend(markerObj.marker.position);
+    });
+
+    this.map.data.forEach(feature => {
+      if (feature.getGeometry().getType() === 'Polygon') {
+        feature.getGeometry().forEachLatLng(function(latlng) {
+          bounds.extend(latlng);
+        });
+      }
+    });
+
+    this.map.fitBounds(bounds);
+  }
+
+  // Draw the pickup areas polygons on the map (if they exist)
+  _drawPickupAreas(data) {
+    if (!this.map) {
+      throw new Error('You must call createMap before attempting to draw pickup area features.');
+    }
+
+     // Attempt to find the pickup areas polygons from the data provided
+     if (data.site && data.site.pickup_areas) {
+      let pickupAreasFeatures = data.site.pickup_areas;
+      this._pickupAreaFeatureIds = pickupAreasFeatures.map(area => area.id);
+
+      if (!Array.isArray(pickupAreasFeatures)) {
+        console.error('The pickup_areas property must be an array of geojson features.');
+        return;
+      }
+      else if (pickupAreasFeatures.length === 0) {
+        console.error('Error! Pickup areas array was blank. Cannot draw polygons.');
+        return;
+      }
+
+      const pickupAreasJson = {'type': 'FeatureCollection', 'features': pickupAreasFeatures};
+
+      if (this.provider === 'google') {
+        this.map.data.addGeoJson(pickupAreasJson);
+      }
+      else {
+        console.error('Adding pickup zones for provider not yet supported');
+      }
+    }
+  }
+
+  _styleMap() {
+    if (!this.map) {
+      throw new Error('You must call createMap before you atttempt to call _styleMap');
+    }
+
+    if (this.provider === 'google') {
+      this.map.data.setStyle(feature => {
+        // Style the premises feature
+        if (feature.getId() === this._premisesFeatureId) {
+          return {
+            strokeColor: '#FFCC00',
+            strokeWeight: 3,
+            fillOpacity: 0
+          }
+        }
+        // Style the pickup areas
+        else if (this._pickupAreaFeatureIds.includes(feature.getId())) {
+          return {
+            fillColor: '#FFCC00',
+            fillOpacity: 0.25,
+            strokeColor: '#FFCC00',
+            strokeOpacity: 0.5,
+            strokeWeight: 5
+          }
+        }
+      });
+    }
+    else {
+      throw new Error('Styling the map is not supported yet for this provider');
+    }
+  }
+
+  _createMarker(latitude, longitude, map) {
+    let lat = parseFloat(latitude);
+    let lng = parseFloat(longitude);
+
+    if (isNaN(lat) || isNaN(lng)) {
+      throw new Error('Latitude or longitude not a valid float');
+    }
+
+    if (this.provider === 'google') {
+      let position = {lat, lng};
+      let marker = new google.maps.Marker({map: map, position: position, draggable: false});
+      return marker;
+    }
+    else {
+      throw new Error('Creating marker for provider not yet supported');
+    }
+  }
+
+  _findMarkerByOrderId(orderId) {
+    return this.markers.find(record => record.orderId === orderId);
+  }
+
+  _moveMarker(marker, latitude, longitude) {
+    let lat = parseFloat(latitude);
+    let lng = parseFloat(longitude);
+
+    if (this.provider === 'google') {
+      let latLng = new google.maps.LatLng(lat, lng);
+      marker.setPosition(latLng);
+    }
+    else {
+      throw new Error('Moving marker for provider not yet supported');
+    }
+  }
+}
